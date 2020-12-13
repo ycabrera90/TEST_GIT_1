62e3eddc55459afafede309c3ef0251898aa5e5f
Yosniel Cabrera <yosniel.ch@gmail.com>
2020-12-13 19:23:23 -0300

    sixth line

--- file1.py
+++ file1.py
@@ -3,3 +3,4 @@ print ('Esta es la segunda linea de mi proyecto');
 print ('Esta es la tercera linea de mi proyecto');
 print ('Esta es la cuarta linea de mi proyecto');
 print ('Esta es la quita linea de mi proyecto');
+print ('Esta es la sexta linea de mi proyecto');
