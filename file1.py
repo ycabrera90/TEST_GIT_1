6cac1e4f13ed0405b5424bafaea37a4f40092827
Yosniel Cabrera <yosniel.ch@gmail.com>
2020-12-13 19:00:47 -0300

    fifth line

--- file1.py
+++ file1.py
@@ -2,3 +2,4 @@ print ('ESTO ES UN ARCHIVO DE PRUEBA');
 print ('Esta es la segunda linea de mi proyecto');
 print ('Esta es la tercera linea de mi proyecto');
 print ('Esta es la cuarta linea de mi proyecto');
+print ('Esta es la quita linea de mi proyecto');
