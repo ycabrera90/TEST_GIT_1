c1dda5d1a9714f0a03c80aa7bb7a21d947ca2aa8
Yosniel Cabrera <yosniel.ch@gmail.com>
2020-12-13 22:49:34 -0300

    Revert "insertion of the second line of my project"
    This reverts commit 431a5d8179d6ab30e34d16684a6f0a16fd33075f.

--- file1.py
+++ file1.py
@@ -1,9 +1,1 @@
-print ('ESTO ES UN ARCHIVO DE PRUEBA')
-print ('Esta es la segunda linea de mi proyecto')
-print ('Esta es la tercera linea de mi proyecto')
-print ('Esta es la cuarta linea de mi proyecto')
-print ('Esta es la quita linea de mi proyecto')
-print ('Esta es la sexta linea de mi proyecto')
-print ('Esta es la seven linea de mi proyecto')
-
-print ('Esta es la octava linea de mi proyecto')
+print ('ESTO ES UN ARCHIVO DE PRUEBA');
