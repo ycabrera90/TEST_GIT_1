ecacff90786c3aee8d853811cc748d81a65add08
Yosniel Cabrera <yosniel.ch@gmail.com>
2020-12-13 22:20:53 -0300

    Revert "the 8th and 3rd line in file 1 and 2"
    This reverts commit 093304d699fad5a7aab8b8514b50fb46236f35aa.

--- file2.py
+++ file2.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 print ('ESTO ES OTRO ARCHIVO DE PRUEBA')
 print ('Esta es la segunda linea de mi proyecto')
 
 print ('Esta es la tercera linea de mi proyecto')
+=======
+print ('ESTO ES OTRO ARCHIVO DE PRUEBA');
+print ('Esta es la segunda linea de mi proyecto');
+>>>>>>> parent of 093304d... the 8th and 3rd line in file 1 and 2
