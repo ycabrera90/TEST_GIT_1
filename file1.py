971882201299834881dc9ad0e6e3e1f4c6ca9275
ycabrera90 <68780363+ycabrera90@users.noreply.github.com>
2020-12-19 19:37:00 -0300

    test1
    test1
    
    change for check the command rebase
    
    prueba de agrupar commit con rebase

--- file1.py
+++ file1.py
@@ -1,17 +1,1 @@
-print ('ESTO ES UN ARCHIVO DE PRUEBA')
-print ('Esta es la segunda linea de mi proyecto')
-print ('Esta es la tercera linea de mi proyecto')
-print ('Esta es la cuarta linea de mi proyecto')
-print ('Esta es la quita linea de mi proyecto')
-print ('Esta es la sexta linea de mi proyecto')
-print ('Esta es la seven linea de mi proyecto')
-
-print ('Esta es la octava linea de mi proyecto')
-# hago unos cambios en la rama master para el archivo 1
-# hago un cambio en el file 1 en la rama test1
-# cambio realizado en git 1
-# cambio que hago en git 1
-# OTRO CAMBIO EN GIT 1
-# OTROoo CAMBIO EN GIT 1
-# OTROoo CAMBIO EN GIT 1
-# Este cambio es en git 1 
+Esta es la prueba para ver el funcionamiento del comando rebase
